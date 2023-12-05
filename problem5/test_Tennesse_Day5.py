25ece3bfb5a4dcb2b4494c02ada807f7cd6a2c3b
tennessejoyce <tuckerj229@gmail.com>
2023-12-05 13:09:09 -0500

    Part 2

--- problem5/test_Tennesse_Day5.py
+++ problem5/test_Tennesse_Day5.py
@@ -1,30 +1,50 @@
 import unittest
-from Tennesse_Day5 import apply_map
+from Tennesse_Day5 import map_int, Interval, map_intervals, MapRow
 
 
 class TestApplyMap(unittest.TestCase):
     def test_single_map(self):
-        m = [(5, 10, 2)]
-        self.assertEqual(apply_map(8, m), 8)  # Number not in the range of the map
-        self.assertEqual(apply_map(9, m), 9)  # Number not in the range of the map
-        self.assertEqual(apply_map(10, m), 5)  # Number in the range of the map
-        self.assertEqual(apply_map(11, m), 6)  # Number in the range of the map
-        self.assertEqual(apply_map(12, m), 12)  # Number not in the range of the map
+        m = [MapRow(Interval(10, 11), -5)]
+        self.assertEqual(map_int(8, m), 8)  # Number not in the range of the map
+        self.assertEqual(map_int(9, m), 9)  # Number not in the range of the map
+        self.assertEqual(map_int(10, m), 5)  # Number in the range of the map
+        self.assertEqual(map_int(11, m), 6)  # Number in the range of the map
+        self.assertEqual(map_int(12, m), 12)  # Number not in the range of the map
 
     def test_multiple_maps(self):
-        m = [(5, 10, 2), (20, 15, 5)]
-        self.assertEqual(apply_map(8, m), 8)  # Number not in the range of the map
-        self.assertEqual(apply_map(9, m), 9)  # Number not in the range of the map
-        self.assertEqual(apply_map(10, m), 5)  # Number in the range of the map
-        self.assertEqual(apply_map(11, m), 6)  # Number in the range of the map
-        self.assertEqual(apply_map(12, m), 12)  # Number not in the range of the map
-        self.assertEqual(apply_map(13, m), 13)  # Number not in the range of the map
-        self.assertEqual(apply_map(14, m), 14)  # Number not in the range of the map
-        self.assertEqual(apply_map(15, m), 20)  # Number not in the range of the map
-        self.assertEqual(apply_map(16, m), 21)  # Number not in the range of the map
-        self.assertEqual(apply_map(19, m), 24)  # Number not in the range of the map
-        self.assertEqual(apply_map(20, m), 20)  # Number not in the range of the map
-        self.assertEqual(apply_map(30, m), 30)  # Number not in the range of the map
+        m = [MapRow(Interval(10, 11), -5), MapRow(Interval(15, 19), 5)]
+        self.assertEqual(map_int(8, m), 8)  # Number not in the range of the map
+        self.assertEqual(map_int(9, m), 9)  # Number not in the range of the map
+        self.assertEqual(map_int(10, m), 5)  # Number in the range of the map
+        self.assertEqual(map_int(11, m), 6)  # Number in the range of the map
+        self.assertEqual(map_int(12, m), 12)  # Number not in the range of the map
+        self.assertEqual(map_int(13, m), 13)  # Number not in the range of the map
+        self.assertEqual(map_int(14, m), 14)  # Number not in the range of the map
+        self.assertEqual(map_int(15, m), 20)  # Number not in the range of the map
+        self.assertEqual(map_int(16, m), 21)  # Number not in the range of the map
+        self.assertEqual(map_int(19, m), 24)  # Number not in the range of the map
+        self.assertEqual(map_int(20, m), 20)  # Number not in the range of the map
+        self.assertEqual(map_int(30, m), 30)  # Number not in the range of the map
+
+
+class TestInterval(unittest.TestCase):
+    def test_zero_length_interval(self):
+        interval = Interval(5, 5)
+        self.assertFalse(bool(interval))
+
+    def test_nonzero_length_interval(self):
+        interval = Interval(5, 7)
+        self.assertTrue(bool(interval))
+
+
+class TestMapIntervals(unittest.TestCase):
+    def test_map_intervals(self):
+        intervals = [Interval(5, 10)]
+        rows = [MapRow(Interval(7, 8), 20)]
+        mapped_intervals = map_intervals(intervals, rows)
+        self.assertEqual(
+            mapped_intervals, [Interval(5, 6), Interval(9, 10), Interval(27, 28)]
+        )
 
 
 if __name__ == "__main__":
